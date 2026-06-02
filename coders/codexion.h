7845cd13e5a8217302e490ca96dfa4c1b65b65aa
Ainur Akhmetov <broklya@gmail.com>
2026-06-02 12:24:56 +0200

    Update header

--- coders/codexion.h
+++ coders/codexion.h
@@ -6,7 +6,7 @@
 /*   By: aakhmeto <aakhmeto@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 18:20:54 by aakhmeto          #+#    #+#             */
-/*   Updated: 2026/05/23 23:01:58 by aakhmeto         ###   ########.fr       */
+/*   Updated: 2026/06/01 16:54:24 by aakhmeto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,25 @@ typedef struct s_args
 	int		error_type;
 }	t_args;
 
+typedef struct s_simulation
+{
+	t_args	args;
+	long	start_time;
+}	t_simulation;
+
 t_args	new_args(void);
 void	parse_args(int argc, char **argv, t_args *args);
 int		ft_str_to_num(const char *num);
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	error_message(int error_type);
+
+long	get_time_ms(void);
+long	get_elapsed_ms(long start_time);
+void	precise_sleep(long duration_ms);
+
+void	log_message(long start_time, int coder_id, char *message);
+
+t_simulation	new_simulation(t_args args);
 
 #endif
