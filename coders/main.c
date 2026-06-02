278429ef8f5a0e33544c4c987afb8ab91b60f39f
Ainur Akhmetov <broklya@gmail.com>
2026-06-02 12:24:15 +0200

    Update main.c

--- coders/main.c
+++ coders/main.c
@@ -6,7 +6,7 @@
 /*   By: aakhmeto <aakhmeto@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 18:20:15 by aakhmeto          #+#    #+#             */
-/*   Updated: 2026/05/23 23:44:59 by aakhmeto         ###   ########.fr       */
+/*   Updated: 2026/06/01 17:09:35 by aakhmeto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	main(int argc, char **argv)
 {
-	t_args	args;
+	t_args			args;
+	t_simulation	simulation;
 
 	args = new_args();
 	parse_args(argc, argv, &args);
@@ -23,5 +24,7 @@ int	main(int argc, char **argv)
 		error_message(args.error_type);
 		return (1);
 	}
+	simulation = new_simulation(args);
+	(void)simulation;
 	return (0);
 }
