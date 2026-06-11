e9fab4c32cd7f05dae12c8a48e7418ff4b0ce5b8
Ainur Akhmetov <broklya@gmail.com>
2026-06-11 12:08:24 +0200

    Improve scheduler heap operations

--- coders/scheduler.c
+++ coders/scheduler.c
@@ -6,7 +6,7 @@
 /*   By: aakhmeto <aakhmeto@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 14:05:51 by aakhmeto          #+#    #+#             */
-/*   Updated: 2026/06/08 14:17:22 by aakhmeto         ###   ########.fr       */
+/*   Updated: 2026/06/09 16:00:00 by aakhmeto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,43 +21,41 @@ static void	swap_requests(t_request *first, t_request *second)
 	*second = temporary;
 }
 
-static int	has_priority(t_simulation *simulation,
-	t_request *first, t_request *second)
+static void	shift_request_up(t_simulation *simulation, int index)
 {
-	if (ft_strncmp(simulation->args.scheduler, "edf", 3) == 0)
+	t_request_heap	*heap;
+	int				parent;
+
+	heap = &simulation->request_heap;
+	while (index > 0)
 	{
-		if (first->deadline != second->deadline)
-			return (first->deadline < second->deadline);
+		parent = (index - 1) / 2;
+		if (!request_has_priority(simulation, &heap->requests[index],
+				&heap->requests[parent]))
+			break ;
+		swap_requests(&heap->requests[index], &heap->requests[parent]);
+		index = parent;
 	}
-	return (first->arrival_order < second->arrival_order);
 }
 
 int	request_heap_insert(t_simulation *simulation, t_coder *coder)
 {
 	t_request_heap	*heap;
-	t_request		request;
+	t_request		*request;
 	int				index;
-	int				parent;
 
 	heap = &simulation->request_heap;
 	if (heap->size >= heap->capacity)
 		return (1);
-	request.coder = coder;
-	request.arrival_order = heap->next_arrival_order++;
-	request.deadline = get_last_compile_start(coder)
+	index = heap->size++;
+	request = &heap->requests[index];
+	request->coder = coder;
+	request->arrival_order = heap->next_arrival_order++;
+	request->deadline = get_last_compile_start(coder)
 		+ simulation->args.time_to_burnout;
-	index = heap->size;
-	heap->requests[index] = request;
-	heap->size++;
-	while (index > 0)
-	{
-		parent = (index - 1) / 2;
-		if (!has_priority(simulation, &heap->requests[index],
-				&heap->requests[parent]))
-			break ;
-		swap_requests(&heap->requests[parent], &heap->requests[index]);
-		index = parent;
-	}
+	request->considered = 0;
+	request->selected = 0;
+	shift_request_up(simulation, index);
 	return (0);
 }
 
@@ -77,43 +75,4 @@ void	destroy_request_heap(t_simulation *simulation)
 	free(simulation->request_heap.requests);
 	simulation->request_heap.requests = NULL;
 	simulation->request_heap.size = 0;
-}
-
-t_coder	*request_heap_remove(t_simulation *simulation)
-{
-	t_request_heap	*heap;
-	t_coder			*winner;
-	int				index;
-	int				child;
-
-	heap = &simulation->request_heap;
-	if (heap->size == 0)
-		return (NULL);
-	winner = heap->requests[0].coder;
-	heap->size--;
-	if (heap->size == 0)
-		return (winner);
-	heap->requests[0] = heap->requests[heap->size];
-	index = 0;
-	while (index * 2 + 1 < heap->size)
-	{
-		child = index * 2 + 1;
-		if (child + 1 < heap->size
-			&& has_priority(simulation, &heap->requests[child + 1],
-				&heap->requests[child]))
-			child++;
-		if (!has_priority(simulation, &heap->requests[child],
-				&heap->requests[index]))
-			break ;
-		swap_requests(&heap->requests[index], &heap->requests[child]);
-		index = child;
-	}
-	return (winner);
-}
-
-t_coder	*request_heap_peek(t_simulation *simulation)
-{
-	if (simulation->request_heap.size == 0)
-		return (NULL);
-	return (simulation->request_heap.requests[0].coder);
 }
---
+++ coders/scheduler_remove.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scheduler_remove.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakhmeto <aakhmeto@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/09 15:12:45 by aakhmeto          #+#    #+#             */
+/*   Updated: 2026/06/09 15:12:49 by aakhmeto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+static void	swap_requests(t_request *first, t_request *second)
+{
+	t_request	temporary;
+
+	temporary = *first;
+	*first = *second;
+	*second = temporary;
+}
+
+static void	shift_up(t_simulation *simulation, int index)
+{
+	t_request_heap	*heap;
+	int				parent;
+
+	heap = &simulation->request_heap;
+	while (index > 0)
+	{
+		parent = (index - 1) / 2;
+		if (!request_has_priority(simulation, &heap->requests[index],
+				&heap->requests[parent]))
+			break ;
+		swap_requests(&heap->requests[index], &heap->requests[parent]);
+		index = parent;
+	}
+}
+
+static void	shift_down(t_simulation *simulation, int index)
+{
+	t_request_heap	*heap;
+	int				child;
+
+	heap = &simulation->request_heap;
+	while (index * 2 + 1 < heap->size)
+	{
+		child = index * 2 + 1;
+		if (child + 1 < heap->size && request_has_priority(simulation,
+				&heap->requests[child + 1], &heap->requests[child]))
+			child++;
+		if (!request_has_priority(simulation, &heap->requests[child],
+				&heap->requests[index]))
+			break ;
+		swap_requests(&heap->requests[index], &heap->requests[child]);
+		index = child;
+	}
+}
+
+void	request_heap_remove_coder(t_simulation *simulation, t_coder *coder)
+{
+	t_request_heap	*heap;
+	int				index;
+	int				parent;
+
+	heap = &simulation->request_heap;
+	index = 0;
+	while (index < heap->size && heap->requests[index].coder != coder)
+		index++;
+	if (index == heap->size)
+		return ;
+	heap->size--;
+	if (index == heap->size)
+		return ;
+	heap->requests[index] = heap->requests[heap->size];
+	parent = (index - 1) / 2;
+	if (index > 0 && request_has_priority(simulation,
+			&heap->requests[index], &heap->requests[parent]))
+		shift_up(simulation, index);
+	else
+		shift_down(simulation, index);
+}
